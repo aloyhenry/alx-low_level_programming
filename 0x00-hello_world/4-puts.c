1deee4e3be9d1650f6fdd4428697b6b92488542a
aloyhenry <henryaloysius4@gmail.com>
2022-09-08 15:50:51 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include<stdio.h>
-using namespace std;
+
 int main()
 {
 	//string initialisation
-    char Mystr[] = ;
     
-    puts("programming is like building a multiligual puzzle"); //writing the string to stdout
+    puts("\"programming is like building a multiligual puzzle"); //writing the string to stdout
     
     return 0;
 }
