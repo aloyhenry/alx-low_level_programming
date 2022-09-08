3d29a7f8ca1373460d918e2d9ac46251b0762a5a
aloyhenry <henryaloysius4@gmail.com>
2022-09-08 16:57:50 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,10 +6,8 @@
  * this program prints "Programming is like building a muliligual puzzle
  * Return: 0
  */
-
 int main(void)
-
 {
-		puts("\"Programming is like building a multilingual puzzle");        
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");        
+	return (0);
 }
