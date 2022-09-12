1828ede8c11abd326be6cff3ec39b988faa1ea55
aloyhenry <henryaloysius4@gmail.com>
2022-09-12 07:26:56 -0700

    printf

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
  * main - prints numbers 0-9 without using char or printf/puts,
  * and putchar 2x max
-(*
+ **
  * Return: 0 on success
  */
+
 int main(void)
 {
         int i = '0';
@@ -15,5 +19,6 @@ int main(void)
                 i++;
         }
         putchar('\n');
+
         return (0);
 }
