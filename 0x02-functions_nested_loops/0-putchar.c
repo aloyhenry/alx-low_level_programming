06e5000ae85d7bb1e335ecb7a973a03acd8bf46d
aloyhenry <henryaloysius4@gmail.com>
2022-09-14 15:17:03 -0700

    nested

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,19 +4,17 @@
  * Description: You are not allowed to include standard libraries.
  * Return: 0
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c'):
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char holberton[] = "_putchar";
+	int i = 0;
+
+	while (holberton[i] != '\0')
+	{
+		_putchar(holberton[i]);
+		i++;
+	}
 	_putchar('\n');
 
-	return (0)
-
+	return (0);
 }
