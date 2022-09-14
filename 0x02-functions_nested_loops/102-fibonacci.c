e3de8d7efcbf37899858a9e7dd7f8609c4f38f1e
aloyhenry <henryaloysius4@gmail.com>
2022-09-13 18:14:37 -0700

    nested

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,14 +8,14 @@
  */
 int main(void)
 {
-       int count = 2;
-       long int i = 1, j = 2;
-       long int k;
+        int count = 2;
+        long int i = 1, j = 2;
+        long int k;
 
-       printf("%lu, ", i);
-       while (count <= 50)
-       {
-               if (count == 50)
+        printf("%lu, ", i);
+        while (count <= 50)
+        {
+                if (count == 50)
                 {
                         printf("%lu\n", j);
                 }
@@ -24,11 +24,11 @@ int main(void)
                         printf("%lu, ", j);
                 }
 
-               k = j;
-               j += i;
-               i = k;
-               count++;
-       }
+                k = j;
+                j += i;
+                i = k;
+                count++;
+        }
 
-       return (0);
+        return (0);
 }
