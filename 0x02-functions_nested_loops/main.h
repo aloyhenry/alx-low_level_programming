57a11b851bedb914230241dc30d88d256d2d8028
aloyhenry <henryaloysius4@gmail.com>
2022-09-14 15:18:56 -0700

    nested

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_baver(void);
-void times_table(vold);
+void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
